c4bfb2626e333e6b4aa2ab239a8fd18ed4ac82c9
Jerome Ortega <ortegajerome@gmail.com>
2023-10-01 19:51:27 +0000

    implement left right forward

--- src/models/agent.py
+++ src/models/agent.py
@@ -1,15 +1,29 @@
 class Agent:
-    loc = (0, 0)
+    location = (0, 0)
     points = 0
     orientations = ['right', 'down', 'left', 'up']
     orientation = 0
 
-    # def turn
+    is_dead = False
+    exited = False
+    has_gold = False
 
-    # def forward
+    def grab_gold(self):
+        self.has_gold = True
 
-    # def shoot
+    def set_location(self, location):
+        self.location = location
 
-    # def grab
+    def turn_left(self):
+        self.orientation = (
+            self.orientation-1) % len(self.orientations)
 
-    # climb
+    def turn_right(self):
+        self.orientation = (
+            self.orientation+1) % len(self.orientation)
+
+    def exit(self):
+        self.exited = True
+
+    def is_dead(self):
+        self.dead = True
--- src/models/episode.py
+++ src/models/episode.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+import os
 
 from .environment import Environment
 from .agent import Agent
@@ -11,57 +11,70 @@ class Episode:
     def __init__(self):
         self.environment = Environment(4, 4)
         self.agent = Agent()
+        self.environment.set_agent(self.agent)
+
         self.oldloc = (0, 0)
         self.loc = (0, 0)
         self.orientation_idx = 0
         self.points = 0
+        self.grabbed_gold = False
 
     def run(self):
         print("Running episode...")
 
         action = None
-        while True:
-            percepts = self.environment.get_percepts(
-                self.oldloc, self.loc, action)
-
+        while not self.agent.is_dead() or not self.agent.exited():
             self.environment.print_grid()
-
-            self.points += percepts["points"]
-            if percepts["agent_dead"]:
-                print(f"Agent is dead. Total Points: {self.points}")
-                return
-
-            if percepts["bump"]:
-                self.loc = deepcopy(self.oldloc)
-
-            orientation = orientations[self.orientation_idx]
-
-            print(f"Current loc: {self.loc}")
-            print(f"facing: {orientation}")
-            print(f"Points: {self.points}")
+            percepts = self.environment.get_percepts(action)
+            print(f"Current loc: {self.agent.location}")
+            print(f"facing: {self.agent.orientations[self.agent.orientation]}")
+            # print(f"Points: {self.points}")
             print(f"Observed: {percepts}")
             action = input("Enter your action: ")
 
-            if action == 'f':
-                self.oldloc = tuple(list(self.loc))
+            # grabbed_gold = percepts["grabbed_gold"]
+            # if grabbed_gold:
+            #     self.agent.grab_gold()
 
-                if (orientation == 'right'):
-                    self.loc = (self.loc[0], self.loc[1]+1)
-                elif (orientation == 'left'):
-                    self.loc = (self.loc[0], self.loc[1]-1)
-                elif (orientation == 'up'):
-                    self.loc = (self.loc[0]+1, self.loc[1])
-                elif (orientation == 'down'):
-                    self.loc = (self.loc[0]-1, self.loc[1])
-            elif action == 'l':
-                self.orientation_idx = (
-                    self.orientation_idx-1) % len(orientations)
-            elif action == 'r':
-                self.orientation_idx = (
-                    self.orientation_idx+1) % len(orientations)
-            elif action == 'g':
-                pass
-            elif action == 's':
-                pass
-            else:
-                print("Unrecognized action")
+            # # os.system('clear')
+            # self.environment.print_grid()
+
+            # self.points += percepts["points"]
+            # if percepts["agent_dead"]:
+            #     print(f"Agent is dead. Total Points: {self.points} {percepts}")
+            #     return
+
+            # if percepts["bump"]:
+            #     self.loc = tuple(list(self.oldloc))
+
+            # orientation = orientations[self.orientation_idx]
+
+            # print(f"Current loc: {self.loc}")
+            # print(f"facing: {orientation}")
+            # print(f"Points: {self.points}")
+            # print(f"Observed: {percepts}")
+            # action = input("Enter your action: ")
+
+            # if action == 'f':
+            #     self.oldloc = tuple(list(self.loc))
+
+            #     if (orientation == 'right'):
+            #         self.loc = (self.loc[0], self.loc[1]+1)
+            #     elif (orientation == 'left'):
+            #         self.loc = (self.loc[0], self.loc[1]-1)
+            #     elif (orientation == 'up'):
+            #         self.loc = (self.loc[0]+1, self.loc[1])
+            #     elif (orientation == 'down'):
+            #         self.loc = (self.loc[0]-1, self.loc[1])
+            # elif action == 'l':
+            #     self.orientation_idx = (
+            #         self.orientation_idx-1) % len(orientations)
+            # elif action == 'r':
+            #     self.orientation_idx = (
+            #         self.orientation_idx+1) % len(orientations)
+            # elif action == 'g':
+            #     pass
+            # elif action == 's':
+            #     pass
+            # else:
+            #     print("Unrecognized action")
