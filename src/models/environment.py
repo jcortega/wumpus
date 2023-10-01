094d672f7689ca2f32896c20e4a66e3003743b5b
Jerome Ortega <ortegajerome@gmail.com>
2023-10-01 01:01:27 +0000

    create environment

--- src/models/environment.py
+++ src/models/environment.py
@@ -1,18 +1,138 @@
 import numpy as np
 
 
+class Room:
+    has_stench = False
+    has_breeze = False
+    has_glitter = False
+    has_wumpus = False
+
+    def __init__(self, has_stench=False, has_breeze=False, has_glitter=False, has_wumpus=False, has_pit=False):
+        self.has_stench = has_stench
+        self.has_breeze = has_breeze
+        self.has_glitter = has_glitter
+        self.has_wumpus = has_wumpus
+        self.has_pit = has_pit
+
+    def put_wumpus(self):
+        self.has_wumpus = True
+
+
 class Environment:
-    def __init__(self):
+    def __init__(self, width=4, height=4):
+        self.gridHeight = height
+        self.gridWidth = width
+
         self.pitProb = 0.2
-        self.gridSize = 4
-        self.grid = np.random.randint(0, 15, (self.gridSize, self.gridSize))
+
+        self.grid = np.empty((self.gridHeight, self.gridWidth), dtype=object)
+        print(self.grid.shape)
+        self.emptyRoom = Room()
+        self.grid.fill(self.emptyRoom)
 
         self.pits = self._make_pits()
+        for p in self.pits:
+            self.grid.flat[p] = Room(has_pit=True)
+
+            left = self.left_idx(p)
+            if left > -1:
+                self.set_breeze(left)
+
+            right = self.right_idx(p)
+            if right > -1:
+                self.set_breeze(right)
+
+            bottom = self.bottom_idx(p)
+            if bottom > -1:
+                self.set_breeze(bottom)
+
+            top = self.top_idx(p)
+            if top > -1:
+                self.set_breeze(top)
+
         self.wumpus = self._make_wumpus()
+        wumpus_loc = self.wumpus[0]
+        self.set_wumpus(wumpus_loc)
+        left = self.left_idx(wumpus_loc)
+        if left > -1:
+            self.set_stench(left)
+
+        right = self.right_idx(wumpus_loc)
+        if right > -1:
+            self.set_stench(right)
+
+        bottom = self.bottom_idx(wumpus_loc)
+        if bottom > -1:
+            self.set_stench(bottom)
+
+        top = self.top_idx(wumpus_loc)
+        if top > -1:
+            self.set_stench(top)
+
         self.gold = self._make_gold()
+        self.set_gold(self.gold[0])
+
+    def set_wumpus(self, index):
+        if (self.grid.flat[index] == self.emptyRoom):
+            self.grid.flat[index] = Room(has_wumpus=True)
+        else:
+            self.grid.flat[index].has_wumpus = True
+
+    def set_gold(self, index):
+        if (self.grid.flat[index] == self.emptyRoom):
+            self.grid.flat[index] = Room(has_glitter=True)
+        else:
+            self.grid.flat[index].has_glitter = True
+
+    def set_breeze(self, index):
+        if self.grid.flat[index] == self.emptyRoom:
+            # create new room
+            self.grid.flat[index] = Room(has_breeze=True)
+        else:
+            # update room element
+            self.grid.flat[index].has_breeze = True
+
+    def set_stench(self, index):
+        if self.grid.flat[index] == self.emptyRoom:
+            # create new room
+            self.grid.flat[index] = Room(has_stench=True)
+        else:
+            # update room element
+            self.grid.flat[index].has_stench = True
+
+    def right_idx(self, index):
+        # returns flat index next to index if exists
+        right = index + 1
+        if (right % self.gridWidth) == 0:
+            return -1
+        return right
+
+    def left_idx(self, index):
+        # returns flat index next to index if exists
+        if (index % self.gridWidth) == 0:
+            return -1
+        return index - 1
+
+    def top_idx(self, index):
+        """
+        returns flat index to the top of the given index if exists
+        """
+        top = index + self.gridWidth
+        if (top > self.gridHeight * self.gridWidth - 1):
+            return -1
+        return top
+
+    def bottom_idx(self, index):
+        """
+        returns flat index to the bottom of the givenindex if exists
+        """
+        bottom = index - self.gridWidth
+        if (bottom < 0):
+            return -1
+        return bottom
 
     def pit_count(self):
-        return int(self.pitProb * (self.gridSize * self.gridSize - 1))
+        return int(self.pitProb * (self.gridHeight * self.gridWidth - 1))
 
     def _make_pits(self):
         cellsChoices = np.arange(1, self.grid.size)
@@ -31,36 +151,42 @@ class Environment:
         return np.random.choice(cellsChoices, 1, replace=False)
 
     def is_pit(self, index):
-        flatIndex = index[0]*self.gridSize+index[1]
-        # print(flatIndex, index, self.pits)
-        return flatIndex in self.pits
+        return self.grid.item(index).has_pit
 
     def is_wumpus(self, index):
-        flatIndex = index[0]*self.gridSize+index[1]
-        # print(flatIndex, index, self.pits)
-        return flatIndex in self.wumpus
+        return self.grid.item(index).has_wumpus
 
     def is_gold(self, index):
-        flatIndex = index[0]*self.gridSize+index[1]
-        # print(flatIndex, index, self.pits)
-        return flatIndex in self.gold
+        return self.grid.item(index).has_glitter
 
-    def print_grid(self, area=4):
+    def is_breeze(self, index):
+        return self.grid.item(index).has_breeze
+
+    def is_stench(self, index):
+        return self.grid.item(index).has_stench
+
+    def print_grid(self):
         print(self.pits, self.wumpus)
-        size = self.gridSize
+        print(self.grid)
         # Reverse range so 0,0 is at the bottom
-        for i in reversed(range(size)):
-            print(("+" + "- " * area) * size + "+")
+        for i in reversed(range(self.gridHeight)):
+            padding = 8
+            print(("+" + "-" * padding) * self.gridWidth + "+")
             row = "|"
-            for j in range(area):
+            for j in range(self.gridWidth):
                 # item = self.grid.item((i, j))
-                element = ' '
+                element = ''
                 if self.is_pit((i, j)):
-                    element = 'P'
-                elif self.is_wumpus((i, j)):
-                    element = 'W'
-                elif self.is_gold((i, j)):
-                    element = 'G'
-                row += f"   {element}    |"
+                    element += 'P'
+                if self.is_wumpus((i, j)):
+                    element += 'W'
+                if self.is_gold((i, j)):
+                    element += 'G'
+                if self.is_breeze((i, j)):
+                    element += 'B'
+                if self.is_stench((i, j)):
+                    element += 'S'
+                row += ' ' * int(padding/2) + element + \
+                    ' ' * (int(padding/2)-len(element)) + "|"
             print(row)
-        print(("+" + "- " * area) * size + "+")
+        print(("+" + "--------") * self.gridWidth + "+")
--- src/models/test_environment.py
+++ src/models/test_environment.py
@@ -3,13 +3,64 @@ import unittest
 from environment import Environment
 
 
-class TestEnvironment(unittest.TestCase):
+class TestSquareEnvironment(unittest.TestCase):
     def setUp(self):
-        self.environment = Environment()
+        self.environment = Environment(width=5, height=4)
 
     def test_print_grid(self):
-        self.environment._make_pits()
         self.environment.print_grid()
+
+    def test_right_idx(self):
+        i = self.environment.right_idx(4)
+        self.assertEqual(i, -1)
+
+        i = self.environment.right_idx(13)
+        self.assertEqual(i, 14)
+
+        i = self.environment.right_idx(5)
+        self.assertEqual(i, 6)
+
+        i = self.environment.right_idx(19)
+        self.assertEqual(i, -1)
+
+    def test_left_idx(self):
+        i = self.environment.left_idx(4)
+        self.assertEqual(i, 3)
+
+        i = self.environment.left_idx(13)
+        self.assertEqual(i, 12)
+
+        i = self.environment.left_idx(5)
+        self.assertEqual(i, -1)
+
+        i = self.environment.left_idx(19)
+        self.assertEqual(i, 18)
+
+    def test_top_idx(self):
+        i = self.environment.top_idx(4)
+        self.assertEqual(i, 9)
+
+        i = self.environment.top_idx(13)
+        self.assertEqual(i, 18)
+
+        i = self.environment.top_idx(5)
+        self.assertEqual(i, 10)
+
+        i = self.environment.top_idx(19)
+        self.assertEqual(i, -1)
+
+    def test_bottom_idx(self):
+        i = self.environment.bottom_idx(4)
+        self.assertEqual(i, -1)
+
+        i = self.environment.bottom_idx(13)
+        self.assertEqual(i, 8)
+
+        i = self.environment.bottom_idx(5)
+        self.assertEqual(i, 0)
+
+        i = self.environment.bottom_idx(19)
+        self.assertEqual(i, 14)
 
 
 if __name__ == '__main__':
